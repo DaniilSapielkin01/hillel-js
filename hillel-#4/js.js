c4173a8f2e15a73ea614478731ac2ddfcc914444
Sapielkin_Daniil <vipdanik98@gmail.com>
2019-09-25 00:50:16 +0300

    25.09+#4

--- hillel-#4/js.js
+++ hillel-#4/js.js
@@ -1,80 +1,177 @@
- // //--- Функции ---
- // //---#1
- // let a = +prompt("#1");
- // let b = +prompt("#2");
+// //--- Функции ---
+// //---#1
+// let a = +prompt("#1");
+// let b = +prompt("#2");
 
- // function max() {
- //   let c = Math.max(a, b);
- //   console.log(c);
- // }
- // max();
+// function max() {
+//   let c = Math.max(a, b);
+//   console.log(c);
+// }
+// max();
 
- // //---#2
+//---#2
+// let arr = [0, -1, 100, 500, 100500];
+// let min = arr[0];
 
- // let a = +prompt("#1");
- // let b = +prompt("#2");
+// function func() {
+//   for (i = 1; i < arr.length; i++) {
+//     if (arr[i] < min) min = arr[i];
+//   }
+//   console.log(min);
+// }
+// func();
 
- // function min() {
- //   let c = Math.min(a, b);
- //   console.log(c);
- // }
- // min();
+// // ---#3
 
- // // ---#3
+// let users = [
+//   {
+//     firstName: "Igor",
+//     lastName: "Rogi",
+//     age: 18
+//   },
+//   {
+//     firstName: "Denis",
+//     lastName: "Sined",
+//     age: 29
+//   },
+//   {
+//     firstName: "Georg",
+//     lastName: "Groeg",
+//     age: 45
+//   },
+//   {
+//     firstName: "Dima",
+//     lastName: "Amid",
+//     age: 14
+//   },
+//   {
+//     firstName: "Klerk",
+//     lastName: "Krelk",
+//     age: 19
+//   },
+//   {
+//     firstName: "Katy",
+//     lastName: "Parry",
+//     age: 20
+//   },
+//   {
+//     firstName: "Inna",
+//     lastName: "Anni",
+//     age: 16
+//   },
+//   {
+//     firstName: "Felix",
+//     lastName: "Xilef",
+//     age: 18
+//   },
+//   {
+//     firstName: "Vladimir",
+//     lastName: "Rimidal",
+//     age: 14
+//   }
+// ];
+// function newUser() {
+//   let filterUsers;
+//   filterUsers = users.filter(function({ age }) {
+//     return age >= 18;
+//   });
+//   filterUsers.forEach(function(item, i, filterUsers) {
+//     item.fullName = item.firstName + " " + item.lastName;
 
- let users = [{
-     firstName: "Igor",
-     lastName: "Rogi",
-     age: 17
-   },
-   {
-     firstName: "Denis",
-     lastName: "Sined",
-     age: 29
-   },
-   {
-     firstName: "Georg",
-     lastName: "Groeg",
-     age: 45
-   },
-   {
-     firstName: "Dima",
-     lastName: "Amid",
-     age: 14
-   },
-   {
-     firstName: "Klerk",
-     lastName: "Krelk",
-     age: 19
-   },
-   {
-     firstName: "Katy",
-     lastName: "Parry",
-     age: 20
-   },
-   {
-     firstName: "Inna",
-     lastName: "Anni",
-     age: 16
-   },
-   {
-     firstName: "Felix",
-     lastName: "Xilef",
-     age: 23
-   },
-   {
-     firstName: "Vladimir",
-     lastName: "Rimidal",
-     age: 14
-   }
- ];
+//     console.log(item);
+//   });
+// }
+// newUser();
 
- let user = prompt("sdfsf")
- // let filterUsers = users.filter(function ({
- //   age
- // }) {
- //   return age > 18;
+//---#4
+// let mus = [1, 2, 3, 4];
+// function func() {
+//   let musDel = mus.splice(0, 1);
+//   console.log(mus);
+//   console.log(musDel);
+// }
+// func();
 
- // });
+//---#5
+// let mus = [1, 2, 3, 4, 1];
+// let value = prompt("");
+// let total;
 
- // console.log(filterUsers);
+// function func() {
+//   newMus = mus.concat(value);
+//   total = newMus.length;
+//   let lastTotal = newMus[total - 1];
+
+//   console.log(lastTotal);
+//   console.log(total);
+// }
+// func();
+
+//---#6
+
+//Object.assign()
+
+//---#7
+//Date list
+// let date = new Date(prompt("Date: yyyy-mm-dd"));
+// let dateYear = date.getFullYear();
+// let dateMonth = date.getMonth() + 1;
+// let dateDay = date.getDate();
+// let allDate = dateYear + "-" + dateMonth + "-" + dateDay;
+//---
+// let messenge = prompt("Messenger");
+// let author;
+
+// function setComment(allDate, messenge, author) {
+//   if (dateYear && messenge) {
+//     author = prompt("Author");
+//     if (author) {
+//     } else {
+//       author = "Anonymous";
+//     }
+//     console.log(
+//       author,
+//       allDate,
+//       `
+//      `,
+//       messenge
+//     );
+//   } else {
+//     console.log("not work");
+//   }
+// }
+// setComment(allDate, messenge, author);
+
+// Замыкание
+//---#8
+// function createTimer() {
+//   for (i = 0; i < 100; i++) {
+//     console.log(i + "  !");
+//   }
+//   return function timer() {
+//     createTimer = performance.now();
+//     console.log(createTimer);
+//   };
+// }
+
+// var timer = createTimer();
+// console.log("!"); // код, время выполнения которого нужно измерить
+// // Тут  undefined на 159 строке не понятно почему
+// console.log(timer());
+// время в мкс от начала выполнения createTimer() до момента вызова timer()
+//---#9
+// function createAdder(name) {
+//   console.log(name + hello + " <=");
+
+//   return function hello(nam) {
+
+//   }
+// }
+
+// var hello = createAdder("Hello, ");
+// console.log(hello("John")); // Hello, John
+// console.log(hello("Harry")); // Hello, Harry
+
+// var plus = createAdder(5);
+// alert(plus(1)); // 6
+// alert(plus(5)); // 10
